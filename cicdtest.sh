5669c3665557fb44c9a3a94ab848f1141d8d8e36
patilajayv <90689474+patilajayv@users.noreply.github.com>
2023-08-26 17:45:49 +0530

    Add files via upload

---
+++ cicdtest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+v1="https://github.com/patilajayv/CICDpractise"
+v2="/var/www/html"
+fold="CICDpractise"
+# if [ -d "$fold" -a ! -h "$fold" ]
+# cd CICDpractise
+folder_path="/home/ajay/CICDpractise"
+if [ ! -d "$folder_path" ]; then
+    echo "Folder does not exist. Creating..."
+    git clone $v1
+    cd $fold
+    git checkout main
+    git pull
+    cp index.html $v2 
+    echo "after 1st cp"
+    service nginx restart 
+else
+    echo "Folder already exists."
+    cd $fold
+    # echo "cd 123"
+    git checkout main
+    git pull
+    cp index.html $v2 
+    echo "after 2nd cp"
+    service nginx restart 
+fi
+# git checkout dev
+# sudo cp index.html $v2
+# sudo service nginx restart
