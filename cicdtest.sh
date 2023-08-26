14f63332a8c4ca631622e01f22c38778bf849675
patilajayv <90689474+patilajayv@users.noreply.github.com>
2023-08-26 19:23:13 +0530

    Update cicdtest.sh

--- cicdtest.sh
+++ cicdtest.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 v1="https://github.com/patilajayv/CI-CD-Pipeline.git"
 v2="/var/www/html"
-fold="CICDpractise"
+fold="CI-CD-Pipeline"
 # if [ -d "$fold" -a ! -h "$fold" ]
 # cd CICDpractise
-folder_path="/home/ajay/CICDpractise"
+folder_path="/home/ubuntu/CI-CD-Pipeline"
 if [ ! -d "$folder_path" ]; then
     echo "Folder does not exist. Creating..."
     git clone $v1
     cd $fold
     git checkout main
     git pull
-    cp index.html $v2 
+    cp index.html $v2
     echo "after 1st cp"
-    service nginx restart 
+    service nginx restart
+    echo "done"
 else
     echo "Folder already exists."
     cd $fold
     # echo "cd 123"
     git checkout main
     git pull
-    cp index.html $v2 
+    cp index.html $v2
     echo "after 2nd cp"
-    service nginx restart 
+    service nginx restart
+    echo "done"
 fi
 # git checkout dev
 # sudo cp index.html $v2
